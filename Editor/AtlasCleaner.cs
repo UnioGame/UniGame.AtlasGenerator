d22de7dcb2e7656be4f0b0bb860440d9a0f00322
Mefodei <mefodei.link@gmail.com>
2021-12-22 15:17:55 +0300

    - update atlas generator

--- Editor/AtlasCleaner.cs
+++ Editor/AtlasCleaner.cs
@@ -10,26 +10,29 @@ namespace UniModules.UniGame.AtlasGenerator.Editor
     using UnityEditor;
     using UnityEngine;
     
-    public class AtlasCleaner : UnityEditor.AssetModificationProcessor
+    public class AtlasCleaner : AssetModificationProcessor
     {
         public static AssetDeleteResult OnWillDeleteAsset(string assetPath, RemoveAssetOptions options)
         {
+            if (!AtlasGeneratorSettings.Asset.allowPostProcessing)
+                return AssetDeleteResult.DidNotDelete;
+            
             UniEditorProfiler.LogTime(nameof(AtlasCleaner), () => RemoveFromAtlases(assetPath));
             return AssetDeleteResult.DidNotDelete;
         }
 
-        static void RemoveFromAtlases(string assetPath)
+        public static void RemoveFromAtlases(string assetPath)
         {
             var assetPaths = new List<string>();
             if (assetPath.IsFilePath())
-            {
                 assetPaths.Add(assetPath);
-            } 
+            
             else if(Directory.Exists(assetPath))
             {
                 string[] filePaths = Directory.GetFiles(assetPath, "*.*", SearchOption.AllDirectories);
                 assetPaths.AddRange(filePaths);
             }
+            
             foreach (var asset in assetPaths)
             {
                 if (AssetDatabase.GetMainAssetTypeAtPath(asset) == typeof(Texture2D) && Path.GetExtension(asset.ToLower()) != AtlasGeneratorCommands.PsbExtension)
--- Editor/AtlasGeneratorSettings.cs
+++ Editor/AtlasGeneratorSettings.cs
@@ -14,16 +14,15 @@ namespace UniModules.UniGame.AtlasGenerator.Editor
     [GeneratedAssetInfo("AtlasGenerator/Editor")]
     public class AtlasGeneratorSettings : GeneratedAsset<AtlasGeneratorSettings>
     {
-        public bool allowPostProcessing = true;
-        
+        public bool allowPostProcessing = false;
+
         [Tooltip("Is Empty generated atlas should be deleted")]
         public bool deleteEmptyAtlas = false;
 
         [Tooltip("Creates an atlas if the specified atlas doesn't exist.")]
         public bool allowAtlasCreation = false;
 
-        [Tooltip("Root directory to look for sprites. Assets/ is used if left empty.")] 
-        [SerializeField]
+        [Tooltip("Root directory to look for sprites. Assets/ is used if left empty.")] [SerializeField]
         public string spritesRoot = string.Empty;
 
         [Tooltip("Rules for managing imported assets.")]
@@ -31,9 +30,9 @@ namespace UniModules.UniGame.AtlasGenerator.Editor
 #if ODIN_INSPECTOR
         [ListDrawerSettings(HideAddButton = false, Expanded = false, DraggableItems = true, HideRemoveButton = false)]
 #endif
-        
+
 #if ODIN_INSPECTOR_3
-        [Searchable(FilterOptions = SearchFilterOptions.ISearchFilterableInterface,Recursive = true)]
+        [Searchable(FilterOptions = SearchFilterOptions.ISearchFilterableInterface, Recursive = true)]
 #endif
         public List<AtlasGeneratorRule> rules = new List<AtlasGeneratorRule>();
 
@@ -49,10 +48,9 @@ namespace UniModules.UniGame.AtlasGenerator.Editor
 #if ODIN_INSPECTOR
         [Button]
 #endif
-        public void Validate() => OnValidate();
-        
-        private void OnValidate()
+        public void Validate()
         {
+            generatedAtlases.RemoveAll(x => string.IsNullOrEmpty(AssetDatabase.AssetPathToGUID(x)));
             generatedAtlases.Sort(Comparer<string>.Default);
             this.MarkDirty();
         }
