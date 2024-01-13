9774539033f980a55ab920186c8230087072f942
Mefodei <mefodei.link@gmail.com>
2024-01-13 13:29:10 +0700

    - update dependency

--- Editor/AtlasGeneratorAtlasSettings.cs
+++ Editor/AtlasGeneratorAtlasSettings.cs
@@ -1,13 +1,10 @@
-﻿using Sirenix.OdinInspector;
-using UniModules.UniGame.Core.Editor.EditorProcessors;
+﻿using UniModules.UniGame.Core.Editor.EditorProcessors;
 using UniModules.UniGame.GraphicsTools.Editor.SpriteAtlas;
 using UnityEngine;
 
 namespace UniModules.UniGame.AtlasGenerator.Editor
 {
-#if ODIN_INSPECTOR
-#endif
-
+    
     [GeneratedAssetInfo(AtlasGeneratorAtlasConstants.DefaultSettingsPath)]
     public class AtlasGeneratorAtlasSettings : GeneratedAsset<AtlasGeneratorAtlasSettings>
     {
@@ -20,9 +17,6 @@ namespace UniModules.UniGame.AtlasGenerator.Editor
             AtlasSettingsApplicationMode.ApplyOnAtlasCreationOnly;
 
         [Tooltip("Settings that will be applied to generated atlases.")]
-#if ODIN_INSPECTOR
-        [HideLabel]
-#endif
         public SpriteAtlasSettings defaultAtlasSettings = new SpriteAtlasSettings();
     }
 }
